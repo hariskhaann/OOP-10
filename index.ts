10a796ca0caf49b1735e175e7fe79b4b13a1609f
Haris <hariskhaann@gmail.com>
2023-11-08 00:10:29 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,62 @@
+import inquirer from "inquirer"
+
+class Student {
+    name:string
+    constructor(n:string){
+        this.name=n
+    }
+}
+
+class Person {
+    students:Student[]=[]
+
+    addStudent(obj:Student){
+        this.students.push()
+    }
+}
+
+const persons = new Person()
+
+
+const programStart = async(persons: Person)=>{
+    do{
+        console.log("welcome guest")
+
+    const ans = await inquirer.prompt({
+        type:"list",
+        message:"Can you chosse any one person to talk",
+        name:"select",
+        choices:["Self","student"]
+    });
+
+    if(ans.select == "Self"){
+        console.log("Hello I am talking with myself")
+        console.log("talk with student")
+    }
+    if(ans.select == "student"){
+        const ans = await inquirer.prompt({
+            type:"input",
+            message:"Can you choose student which you are talking",
+            name:"student",
+        })
+
+        const student = persons.students.find(val => val.name == ans.student)
+
+        if(!student){
+            const name = new Student(ans.student)
+            persons.addStudent(name)
+
+            console.log(`Hello i am ${name.name}, or i am fine`);
+            console.log(persons.students);
+        }
+
+        if(student){
+            console.log(`Hello i am ${student.name}, or i am fine.........`);
+            console.log(persons.students);
+        }
+
+    }
+    }while(true)
+}
+
+programStart(persons)
